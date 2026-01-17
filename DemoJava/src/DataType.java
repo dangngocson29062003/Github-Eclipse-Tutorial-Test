b2c651a8e45a9d4c7ac94f4593e53ff0cbb528ad
Son Dang <dangngocson29062003@gmail.com>
2026-01-17 11:16:12 +0700

    Array

--- DemoJava/src/DataType.java
+++ DemoJava/src/DataType.java
@@ -2,8 +2,14 @@
 public class DataType {
 
 	public static void main(String[] args) {
-		byte orangePrice = 127;
-		System.out.println(orangePrice);
+		int[] arr = { 4, 6, 7 };
+		String[] arr2 = { "Hello", "World" };
+		for (int i = 0; i <= arr.length - 1; i++) {
+			System.out.println(arr[i]);
+		}
+		for (String i : arr2) {
+			System.out.println(i);
+		}
 	}
 
 }
